22ebbd417590cb5df26b322577c44bbcf7ccb0ee
Rana Ahmed <rana.ahmed@enozom.com>
2017-11-23 12:21:16 +0200

    finish categories page'

--- coder-swag/Controller/CategoriesVC.swift
+++ coder-swag/Controller/CategoriesVC.swift
@@ -15,13 +15,26 @@ class CategoriesVC: UIViewController , UITableViewDelegate, UITableViewDataSourc
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        categoryTableView.dataSource = self
+        categoryTableView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return DataService.instance.getCategories().count
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as? CategoryCell {
+            let category = DataService.instance.getCategories()[indexPath.row]
+            cell.updateView(category: category)
+            
+            //cell.categoryImage?.image = category.categoryImage
+            //cell.categoryLabel.text = category.categoryTitle
+            return cell
+        } else {
+            return CategoryCell()
+        }
+    }
 }
 
--- coder-swag/View/CategoryCell.swift
+++ coder-swag/View/CategoryCell.swift
@@ -13,6 +13,10 @@ class CategoryCell: UITableViewCell {
     @IBOutlet weak var categoryImage: UIImageView!
     @IBOutlet weak var categoryLabel: UILabel!
     
+    func updateView(category: Category) {
+        categoryImage.image = UIImage(named: category.categoryImage)
+        categoryLabel.text = category.categoryTitle
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
